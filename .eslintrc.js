ca41035517a2ed5b7fb64201f5afb171de8f000f
Mauricio Sabogal <mauricio@kommit.co>
2020-06-09 17:28:22 -0500

    Add project config

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'class-methods-use-this': ['off'],
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+    library: 'DataHistory',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+  },
+};
