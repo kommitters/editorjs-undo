6be50a5e5171f5603c71efe1b05fda3131b4485f
Nicolás Gaviria <nicolas.gaviria@kommit.co>
2020-06-23 10:30:59 -0500

    Add docs to plugin and test fixtures

--- test/fixtures/data.js
+++ test/fixtures/data.js
@@ -1,7 +1,13 @@
+/**
+ * Mocks user provided initial data.
+ */
 const initialData = {
   blocks: [{ type: 'paragraph', data: { text: 'First paragraph' } }],
 };
 
+/**
+ * Mocks new data to be recorded at the history stack.
+ */
 const newData = {
   blocks: [
     { type: 'paragraph', data: { text: 'First paragraph' } },
