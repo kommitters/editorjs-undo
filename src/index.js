2216f39a33f00f4faecff2bf755ca1ec139e9ee7
Julián David Sánchez Gallego <50778235+juliansg022@users.noreply.github.com>
2022-01-20 16:25:20 -0500

    3 - Add the basic methods (#12)
    * Update the icon dimensions
    
    * Update the bundle file
    
    * Implement the access to the toggle
    
    * Implement the constructor method
    
    * Implement the render method
    
    * Implement the save method
    
    * Implement the validate method
    
    * Implement styles to the toggle icon and text
    
    * Update the bundle file with the suggested correction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
-const toggle = 'editorjs-toggle-block';
+import './index.css';
+import toggleIconPrimary from '../assets/toggleIcon.svg';
 
-console.log(toggle);
+export default class ToggleBlock {
+  static get toolbox() {
+    return {
+      title: 'Toggle',
+      icon: toggleIconPrimary,
+    };
+  }
+
+  constructor({ data }) {
+    this.data = {
+      text: data.text || '',
+    };
+
+    this.wrapper = undefined;
+  }
+
+  render() {
+    this.wrapper = document.createElement('div');
+    this.wrapper.classList.add('toggle-block');
+    this.wrapper.innerHTML = toggleIconPrimary;
+
+    const input = document.createElement('div');
+
+    input.classList.add('toggle-input');
+    input.contentEditable = true;
+    input.innerHTML = this.data.text || '';
+
+    this.wrapper.appendChild(input);
+
+    return this.wrapper;
+  }
+
+  save(blockContent) {
+    const caption = blockContent.querySelector('div');
+    return Object.assign(this.data, {
+      text: caption.innerHTML,
+    });
+  }
+
+  validate(savedData) {
+    if (!savedData.text.trim()) {
+      return false;
+    }
+    return true;
+  }
+}
