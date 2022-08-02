9578895af958a09d8e52b5f853e4b70e28ab21ba
Santiago Botero <santiago.botero@kommit.co>
2022-08-02 16:33:47 -0500

    Resolve review

--- src/index.js
+++ src/index.js
@@ -327,7 +327,6 @@ export default class Undo {
    * Insert a block when is skipped
    * @param {Number} prevStateLength is the previous state according to this.position.
    * @param {Array} state is the current state according to this.position.
-   * @returns 
    */
   insertSkippedBlocks(prevStateLength, state) {
     for (let i = prevStateLength; i < state.length; i += 1) {
