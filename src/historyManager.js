590a52fb888318ebe420470eabc1745a4022fccc
Julián Sánchez <50778235+juliandsg0222@users.noreply.github.com>
2024-06-21 15:36:10 -0500

    Implement the 'replace' core function
    Co-authored-by: norn <keliumJU@gmail.com>

--- src/historyManager.js
+++ src/historyManager.js
@@ -64,10 +64,25 @@ export default class HistoryManager {
   /**
    * @param {Array} jsonPatchElement - Formatted changes gotten between the state and baseData
    * @param {Object} blocks — API to make operations on the editor blocks
+   * @param {Object} caret — API to send the focus to a specific block in the editor
+   * @param {Object} baseData - Copy of the saved data object.
    * @description Updates blocks in the editor based on the jsonPatch replace operation
   */
-  async replace({ jsonPatchElement, blocks }) {
-    // Actions to replace the jsonPatchElement in the editor
+  async replace({
+    jsonPatchElement,
+    blocks,
+    caret,
+    baseData,
+  }) {
+    const { path } = jsonPatchElement;
+    const index = path.split('/')[1];
+
+    const { id, data } = await blocks.getBlockByIndex(index).save();
+    const { data: oldData } = baseData[index];
+    const updatedData = Object.assign(data, oldData);
+
+    await blocks.update(id, updatedData);
+    await caret.setToBlock(index, 'end');
   }
 
   /**
