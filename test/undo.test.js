ddce2f332031f95eb274f9d4aa1b719918783fce
Julián Sánchez <50778235+juliandsg0222@users.noreply.github.com>
2024-07-04 12:03:30 -0500

    Mock first changes to be recorded at the history stacks, and used to restore the data

--- test/undo.test.js
+++ test/undo.test.js
@@ -9,6 +9,24 @@ const initialData = {
   blocks: [{ id: 'abk2', type: 'paragraph', data: { text: 'First paragraph' } }],
 };
 
+// Mocks first data to be recorded at the history stack.
+const firstChange = {
+  blocks: [
+    { id: 'abk2', type: 'paragraph', data: { text: 'First paragraph' } },
+    { id: 'zyl9', type: 'paragraph', data: { text: 'Second paragraph' } },
+  ],
+  diff: { 1: [{ type: 'paragraph', data: { text: 'Second paragraph' } }], _t: 'a' },
+  formattedChange: [
+    {
+      op: 'replace',
+      path: '/1',
+      value: { id: 'zyl9', type: 'paragraph', data: { text: 'Second paragraph' } },
+    },
+  ],
+  reverse: { _t: 'a', _1: [{ id: 'zyl9', type: 'paragraph', data: { text: 'Second paragraph' } }, 0, 0] },
+  formattedReverse: [{ op: 'remove', path: '/1' }],
+};
+
 jest.mock('jsondiffpatch', () => ({ create: jest.fn() }));
 jest.mock('jsondiffpatch/formatters/jsonpatch', () => jest.fn());
 
