2bc4a3d9054053d459d3da86f5499098f66d0731
Julián Sánchez <50778235+juliandsg0222@users.noreply.github.com>
2024-07-04 11:59:55 -0500

    Mock user provided initial data

--- test/undo.test.js
+++ test/undo.test.js
@@ -4,6 +4,11 @@
 import Undo from '../src/index';
 import { editor, readOnlyEditor, tools } from './fixtures/editor';
 
+// Mocks user provided initial data.
+const initialData = {
+  blocks: [{ id: 'abk2', type: 'paragraph', data: { text: 'First paragraph' } }],
+};
+
 jest.mock('jsondiffpatch', () => ({ create: jest.fn() }));
 jest.mock('jsondiffpatch/formatters/jsonpatch', () => jest.fn());
 
