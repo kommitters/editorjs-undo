4ea374430b5294d955cb7575d082748461ffc109
Julián Sánchez <50778235+juliandsg0222@users.noreply.github.com>
2024-07-04 18:27:43 -0500

    Refactor libraries mocks

--- test/undo.test.js
+++ test/undo.test.js
@@ -2,77 +2,87 @@
  * @jest-environment jsdom
  */
 import Undo from '../src/index';
+import { initialData, firstChange, secondChange } from './fixtures/data';
 import { startDocument, setFocus } from './testHelpers';
 import { editor, readOnlyEditor, tools } from './fixtures/editor';
 
-// Mocks user provided initial data.
-const initialData = {
-  blocks: [{ id: 'abk2', type: 'paragraph', data: { text: 'First paragraph' } }],
+const revolveDiff = (state) => {
+  switch (state) {
+    case firstChange.blocks:
+      return firstChange.diff;
+
+    case secondChange.blocks:
+      return secondChange.diff;
+
+    default:
+      return undefined;
+  }
 };
 
-// Mocks first data to be recorded at the history stack.
-const firstChange = {
-  blocks: [
-    { id: 'abk2', type: 'paragraph', data: { text: 'First paragraph' } },
-    { id: 'zyl9', type: 'paragraph', data: { text: 'Second paragraph' } },
-  ],
-  diff: { 1: [{ type: 'paragraph', data: { text: 'Second paragraph' } }], _t: 'a' },
-  formattedChange: [
-    {
-      op: 'replace',
-      path: '/1',
-      value: { id: 'zyl9', type: 'paragraph', data: { text: 'Second paragraph' } },
-    },
-  ],
-  reverse: { _t: 'a', _1: [{ id: 'zyl9', type: 'paragraph', data: { text: 'Second paragraph' } }, 0, 0] },
-  formattedReverse: [{ op: 'remove', path: '/1' }],
+const revolveReverse = (lastState) => {
+  switch (lastState) {
+    case firstChange.diff:
+      return firstChange.reverse;
+
+    case secondChange.diff:
+      return secondChange.reverse;
+
+    default:
+      return undefined;
+  }
+};
+
+const resolveFormat = (lastState) => {
+  switch (lastState) {
+    case firstChange.diff:
+      return firstChange.formattedChange;
+
+    case firstChange.reverse:
+      return firstChange.formattedReverse;
+
+    case secondChange.diff:
+      return secondChange.formattedChange;
+
+    case secondChange.reverse:
+      return secondChange.formattedReverse;
+
+    default:
+      return undefined;
+  }
+};
+
+const resolveApplyPatch = (baseData, jsonPatch) => {
+  switch (jsonPatch) {
+    case firstChange.formattedReverse:
+      return { doc: initialData.blocks };
+
+    case firstChange.formattedChange:
+      return { doc: firstChange.blocks };
+
+    case secondChange.formattedReverse:
+      return { doc: firstChange.blocks };
+
+    case secondChange.formattedChange:
+      return { doc: secondChange.blocks };
+
+    default:
+      return { doc: baseData };
+  }
 };
 
 jest.mock('jsondiffpatch', () => ({
   create: () => ({
-    diff: (_baseData, state) => {
-      if (state === firstChange.blocks) {
-        return firstChange.diff;
-      }
-      return undefined;
-    },
-    reverse: (lastState) => {
-      if (lastState === firstChange.diff) {
-        return firstChange.reverse;
-      }
-      return undefined;
-    },
+    diff: (_baseData, state) => revolveDiff(state),
+    reverse: (lastState) => revolveReverse(lastState),
   }),
 }));
 
 jest.mock('jsondiffpatch/formatters/jsonpatch', () => ({
-  format: (lastState) => {
-    switch (lastState) {
-      case firstChange.diff:
-        return firstChange.formattedChange;
-
-      case firstChange.reverse:
-        return firstChange.formattedReverse;
-
-      default:
-        return undefined;
-    }
-  },
+  format: (lastState) => resolveFormat(lastState),
 }));
 
 jest.mock('json-joy/lib/json-patch', () => ({
-  applyPatch: (baseData, jsonPatch) => {
-    switch (jsonPatch) {
-      case firstChange.formattedReverse:
-        return { doc: initialData.blocks };
-
-      case firstChange.formattedChange:
-        return { doc: firstChange.blocks };
-
-      default:
-        return { doc: baseData };
-    }
-  },
+  applyPatch: (baseData, jsonPatch) => resolveApplyPatch(baseData, jsonPatch),
 }));
 
 describe('Undo', () => {
