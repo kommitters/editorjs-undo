8104e3dcbb1590a34bea17a5854521011dea0c07
Julián Sánchez <50778235+juliandsg0222@users.noreply.github.com>
2024-07-04 18:23:44 -0500

    Move mocks of data to separate file

--- test/fixtures/data.js
+++ test/fixtures/data.js
@@ -1,28 +1,55 @@
-/**
- * Mocks user provided initial data.
- */
+// Mocks user provided initial data.
 const initialData = {
-  blocks: [{ type: 'paragraph', data: { text: 'First paragraph' } }],
+  blocks: [{ id: 'abk2', type: 'paragraph', data: { text: 'First paragraph' } }],
 };
 
-/**
- * Mocks first data to be recorded at the history stack.
- */
+// Mocks first data to be recorded at the history stack.
 const firstChange = {
   blocks: [
-    { type: 'paragraph', data: { text: 'First paragraph' } },
-    { type: 'paragraph', data: { text: 'Second paragraph' } },
+    { id: 'abk2', type: 'paragraph', data: { text: 'First paragraph' } },
+    { id: 'zyl9', type: 'paragraph', data: { text: 'Second paragraph' } },
   ],
+  diff: { 1: [{ id: 'zyl9', type: 'paragraph', data: { text: 'Second paragraph' } }], _t: 'a' },
+  formattedChange: [
+    {
+      op: 'replace',
+      path: '/1',
+      value: { id: 'zyl9', type: 'paragraph', data: { text: 'Second paragraph' } },
+    },
+  ],
+  reverse: { _t: 'a', _1: [{ id: 'zyl9', type: 'paragraph', data: { text: 'Second paragraph' } }, 0, 0] },
+  formattedReverse: [{ op: 'remove', path: '/1' }],
 };
 
-/**
- * Mocks second data to be recorded at the history stack.
- */
+// Mocks second data to be recorded at the history stack.
 const secondChange = {
   blocks: [
-    { type: 'paragraph', data: { text: 'First paragraph' } },
-    { type: 'paragraph', data: { text: 'Second paragraph' } },
-    { type: 'paragraph', data: { text: 'Third paragraph' } },
+    { id: 'abk2', type: 'paragraph', data: { text: 'First paragraph' } },
+    { id: 'zyl9', type: 'paragraph', data: { text: 'Modified paragraph' } },
+    { id: 'it69o', type: 'paragraph', data: { text: 'Third paragraph' } },
+  ],
+  diff: {
+    1: { data: { text: ['Second paragraph', 'Modified paragraph'] } },
+    2: [{ id: 'it69o', type: 'paragraph', data: { text: 'Third paragraph' } }],
+    _t: 'a',
+  },
+  formattedChange: [
+    { op: 'replace', path: '/1/data/text', value: 'Modified paragraph' },
+    {
+      op: 'add',
+      path: '/2',
+      value: { id: 'it69o', type: 'paragraph', data: { text: 'Third paragraph' } },
+    },
+  ],
+  reverse: {
+    1: { data: { text: ['Modified paragraph', 'Second paragraph'] } },
+    _t: 'a',
+    _2: [
+      { id: 'it69o', type: 'paragraph', data: { text: 'Third paragraph' } }, 0, 0],
+  },
+  formattedReverse: [
+    { op: 'remove', path: '/2' },
+    { op: 'replace', path: '/1/data/text', value: 'Second paragraph' },
   ],
 };
 
@@ -42,4 +69,5 @@ export {
   firstChange,
   secondChange,
   newChange,
+
 };
